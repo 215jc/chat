121e0fe5dcdeb37000c91b7ca120ffd4c761be8d
Jason Curran <jc@jasoncurran.com>
2015-10-16 15:32:18 -0400

    heroku example

--- environments.rb
+++ environments.rb
@@ -4,16 +4,7 @@ set :database, "sqlite3:database.sqlite3"
 end
 
 configure :production do
- db = 'postgres://xqatqaaqpsmxby:imbdtwQw5eWgtZcSFY0myJch5C@ec2-54-204-15-48.compute-1.amazonaws.com:5432/d34hh5q9m0com2'
-
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
+ ActiveRecord::Base.establish_connection('postgres://xqatqaaqpsmxby:imbdtwQw5eWgtZcSFY0myJch5C@ec2-54-204-15-48.compute-1.amazonaws.com:5432/d34hh5q9m0com2')
 end
 
 
