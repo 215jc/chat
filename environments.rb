0f2fcf1658138e7c8a3a45b75b6bfe40c7f95cd2
Jason Curran <jc@jasoncurran.com>
2015-10-16 15:54:06 -0400

    heroku example

--- environments.rb
+++ environments.rb
@@ -5,6 +5,7 @@ end
 
 configure :production do
  ActiveRecord::Base.establish_connection('postgres://xqatqaaqpsmxby:imbdtwQw5eWgtZcSFY0myJch5C@ec2-54-204-15-48.compute-1.amazonaws.com:5432/d34hh5q9m0com2')
+  set :show_exceptions, true
 end
 
 
